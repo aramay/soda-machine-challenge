7e778624146166212d0e61b7e4d2c7a98a9ea3ec
Abid Ramay <email": null>
2016-05-09 15:02:03 -0700

    Finished 'sell' method

--- soda_machine.rb
+++ soda_machine.rb
@@ -1,10 +1,10 @@
+# require 'pry-byebug'
 class SodaMachine
   attr_reader :sodas, :cash
 
   def initialize(args = {})
     @sodas = args[:sodas]
     @cash = args[:cash]
-    # p sodas
   end
 
   def current_inventory_count
@@ -16,19 +16,27 @@ class SodaMachine
       if soda.brand == soda_brand
         return @sodas[index]
       end
-
       return nil
     end
-
   end
 
   def sell(soda_brand)
+    # binding.pry
     @sodas.each_with_index do |soda, index|
       if soda.brand == soda_brand
         @cash += 1
       end
-        @sodas.pop(index)
+        # puts "Before pop!!!!!!!!!!!!!!!!!!!"
+        # # p "#{@sodas.pop(index)}"
+        puts @sodas.delete_at(index)
+        puts @sodas
+
+        # p "#{soda.brand}"
+        # puts "After pop"
+        # p "$" * 50
+        # p @sodas
     end
+
     return nil
   end
 
