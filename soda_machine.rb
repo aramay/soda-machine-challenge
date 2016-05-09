b75446ae2aaa8197e2fa921c8647061087d225dd
Abid Ramay <email": null>
2016-05-09 14:36:42 -0700

    Add 'current_investory'/'find_soda' methods

--- soda_machine.rb
+++ soda_machine.rb
@@ -4,15 +4,32 @@ class SodaMachine
   def initialize(args = {})
     @sodas = args[:sodas]
     @cash = args[:cash]
+    # p sodas
   end
 
   def current_inventory_count
+    @sodas.length
   end
 
   def find_soda(soda_brand)
+    @sodas.each_with_index do |soda, index|
+      if soda.brand == soda_brand
+        return @sodas[index]
+      end
+
+      return nil
+    end
+
   end
 
   def sell(soda_brand)
+    @sodas.each_with_index do |soda|
+      if soda.brand == soda_brand
+        @cash += 1
+      end
+      @sodas.pop(soda_brand)
+    end
+    return nil
   end
 
 end
